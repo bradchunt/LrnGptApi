d5e3a2dec2d7e5093e9984595074eab3365f9597
Brad Hunt <bhunt.hunt@brad-hunt.lan>
2023-12-21 09:42:05 -0600

    initial commit

---
+++ Controllers/QuestionController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using LearnosityDotNetHelper;
+
+
+namespace LrnGptApi.Controllers;
+
+
+[ApiController]
+[Route("[controller]")]
+public class QuestionController : ControllerBase
+{
+   
+   [HttpPost(Name ="PostQuestion")]
+   public string Post(GPTQuestion question)
+   {
+      Question lrnQuestion = new()
+      {
+         Type = QuestionTypes.Mcq,
+         Reference = Guid.NewGuid().ToString()
+         
+      };
+
+      lrnQuestion.Data.Stimulus=question.Stimulus;
+      lrnQuestion.Data.Options.Add(new Option(){Label=question.AnswerChoice1,Value=question.AnswerChoice1});
+      lrnQuestion.Data.Options.Add(new Option(){Label=question.AnswerChoice2,Value=question.AnswerChoice2});
+      lrnQuestion.Data.Options.Add(new Option(){Label=question.AnswerChoice3,Value=question.AnswerChoice3});
+      lrnQuestion.Data.Options.Add(new Option(){Label=question.AnswerChoice4,Value=question.AnswerChoice4});
+      lrnQuestion.Data.Type=QuestionTypes.Mcq;
+      lrnQuestion.Data.Validation.ScoringType="exactMatch";
+      lrnQuestion.Data.Validation.ValidResponse.Score=1;
+      lrnQuestion.Data.Validation.ValidResponse.Value.Add(question.CorrectAnswer);
+    
+Meta meta= new();
+meta.User.ID="bradhunt";
+meta.User.FirstName="Brad";
+meta.User.LastName="Hunt";
+meta.User.Email="brad.hunt@learnosity.com";
+
+Questions questions = new();
+questions.Meta=meta;
+questions.Question.Add(lrnQuestion);
+
+Item item = new Item
+{
+    Reference = Guid.NewGuid().ToString(),
+    Status = "published"
+};
+
+      item.QuestionReferences.Add(new QuestionReference(){Reference=lrnQuestion.Reference});
+      item.Definition.Widgets.Add(new Widget(){Reference=lrnQuestion.Reference});
+
+      Items items = new();
+      items.Meta=meta;
+      items.Item.Add(item);
+
+      ItemBank.SetQuestions(questions);
+      ItemBank.SetItems(items);
+
+        return "200";
+   }
+
+
+}
---
+++ Models/Question.cs
@@ -0,0 +1,13 @@
+﻿namespace LrnGptApi;
+
+public class GPTQuestion
+{
+public string? Stimulus { get; set; }
+public string? AnswerChoice1 { get; set; }
+public string? AnswerChoice2 { get; set; }
+public string? AnswerChoice3 { get; set; }
+public string? AnswerChoice4 { get; set; }
+
+public string? CorrectAnswer { get; set; }
+
+}
